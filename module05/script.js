f01223252917e69ee20c26d8d67492315ab7b6de
Hana Enein <hanacat123@gmail.com>
2023-05-06 23:28:47 -0700

    hi

--- module05/script.js
+++ module05/script.js
@@ -2,8 +2,6 @@ var table = prompt("Please enter a number", 3); // Unit of table
 var i = 1;                                      // Set counter to 1
 var msg = '<h2>Multiplication Table</h2>';      // Message
 
-
-
 while (i < 11) {
 msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
 i++;
